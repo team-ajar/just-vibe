b76bd00c24976f0131ceeb87d21d0faeaeefd3fc
jrob112 <josh.roberts0527@gmail.com>
2024-05-31 19:27:38 -0500

    update /added eslint, bable, eslint config, and webpack updates

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,22 @@
 const path = require('path');
 
+// this config can be in webpack.config.js or other file with constants
+const API_URL = {
+  production: JSON.stringify('prod-url'),
+  development: JSON.stringify('dev-url')
+}
+
+// check environment mode
+const environment = process.env.NODE_ENV === 'production' ? 'production' : 'development';
+
+// webpack config
 module.exports = {
-  mode: 'development',
+  mode: environment,
   entry: './client/src/index.tsx',
   devtool: 'inline-source-map',
-  watch: true,
+  watch: {
+    mode: 'development'
+  },
   module: {
     rules: [
       {
@@ -21,4 +33,9 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [
+    new webpack.DefinePlugin({
+        'API_URL': API_URL[environment]
+    })
+],
 };
