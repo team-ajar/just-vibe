38968a3ee545619ae86de0e1a6d7879b26833314
jrob112 <josh.roberts0527@gmail.com>
2024-06-03 19:07:22 -0500

    updated /added the controller files

--- server/routers/index.ts
+++ server/routers/index.ts
@@ -1,2 +1,10 @@
 const express = require('express');
+const searchController = ('./searchController');
+const eventsController = ('./eventsController');
+const profileController = ('./profileController');
+const reviewsController = ('./reviewsController');
 const router = express.Router();
+
+
+
+module.exports = router;
