b844dc13c905dddd7b06921901ddddb617b2a182
Allyn McKinney <allynmckinney@Allyns-MacBook-Pro.local>
2024-06-07 16:15:12 -0500

    read user info

--- server/routers/profileController.ts
+++ server/routers/profileController.ts
@@ -1,41 +1,27 @@
-import express, { Express, Request, Response } from 'express'
-import axios, { AxiosResponse } from 'axios';
+import express, { Express, Request, Response } from "express";
+import axios, { AxiosResponse } from "axios";
 
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
 module.exports = {
-createUser: (req: Request, res: Response) => {
-  /* LAST RESORT
-   make req to '/profile' + use response's data to create user
-   nickname -> username + name
-  sub -> googleId ?
-  locale ->  location
-  */
+  // createUser: (req: Request, res: Response) => {},
 
-  axios.get('/prisma')
-    .then(data => console.log(data))
-    .catch(err => console.error(err));
+  readUser: (req: Request, res: Response) => {
+    prisma.user
+      .findMany()
+      .then((found) => {
+        console.log(found);
+        if (found) {
+          res.status(200).send(found);
+        } else {
+          res.status(404).send("User not found");
+        }
+      })
+      .catch((err) => res.sendStatus(500));
+  },
 
-  // const user = prisma.user.create({
-  //   data: {
-  //     name: '',
-  //     username: '',
-  //     googleId: '',
-  //     location: ''
-  //   }
-  // })
-},
+  updateUser: (req: Request, res: Response) => {},
 
-readUser: (req: Request, res: Response) => {
-  
-},
-
-updateUser: (req: Request, res: Response) => {
-
-},
-
-deleteUser: (req: Request, res: Response) => {
-
-}
+  deleteUser: (req: Request, res: Response) => {},
 };
