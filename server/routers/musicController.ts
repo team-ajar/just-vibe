b2794f31da52c292b2ab3b51a2175aa8adccc107
uhrinuh <nasri.arina@gmail.com>
2024-06-06 19:14:52 -0700

    dynamically associate album with the artist

--- server/routers/musicController.ts
+++ server/routers/musicController.ts
@@ -4,30 +4,69 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 module.exports = {
+  saveArtist: (req: Request, res: Response) => {
+    const { artistName } = req.body;
+
+    prisma.artist.findFirst({
+      where: { name: artistName }
+    })
+      .then((existingArtist) => {
+        if (existingArtist) {
+          console.log('Artist already exists', existingArtist);
+          res.sendStatus(404);
+        } else {
+          // save artist to db
+          return prisma.artist.create({
+            data: {
+              name: artistName,
+              description: 'N/A'
+            }
+          })
+            .then(() => {
+              res.sendStatus(201);
+            })
+            .catch((err) => {
+              console.error('Error saving artist', err);
+              res.sendStatus(500);
+            });
+        }
+      })
+      .catch((err) => {
+        console.error('Error finding artist', err);
+        res.sendStatus(500);
+      });
+  },
+
   saveAlbum: (req: Request, res: Response) => {
     const { albumName, artistId } = req.body;
 
-    // save album to db
-    prisma.album.create({
-      data:{
-        name: albumName,
-        artistId: artistId,
-      }
+    prisma.album.findFirst({
+      where: { name: albumName }
     })
-      .then((data: any) => console.log(data))
-      .catch((err: any) => console.log(err));
-  },
-
-  saveArtist: (req: Request, res: Response) => {
-    const { artistName } = req.body;
-    
-    prisma.artist.create({
-      data: {
-        name: artistName,
-        description: 'N/A'
-      }
-    })
-      .then((data: any) => console.log(data))
-      .catch((err: any) => console.log(err));
+      .then((existingAlbum) => {
+        if (existingAlbum) {
+          console.log('Album already exists', existingAlbum);
+          res.sendStatus(404);
+        } else {
+          // save album to db
+          prisma.album.create({
+            data: {
+              name: albumName,
+              artistId: artistId,
+            }
+          })
+            .then(() => {
+              res.sendStatus(201);
+            })
+            .catch((err) => {
+              console.error('Error saving album', err);
+              res.sendStatus(500);
+            });
+        }
+      })
+      .catch((err) => {
+        console.error('Error finding album', err);
+        res.sendStatus(500);
+      });
   },
 }
