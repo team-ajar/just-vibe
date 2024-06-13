b76b7af352d0f84d59e5ea59606590f5eabc8951
cougarred1 <RodmanLavalais@gmail.com>
2024-06-12 22:09:07 -0500

    ADDED change

--- server/routers/searchController.ts
+++ server/routers/searchController.ts
@@ -33,7 +33,7 @@ module.exports = {
           axios.get(`https://app.ticketmaster.com/discovery/v2/events.json?apikey=${TICKETMASTER_API_KEY}&keyword=${search}`, {headers: {"Content-Type": "application/json"}})
           .then((data: any) => {
           
-          searchResults.events = data.data._embedded.events[0]._embedded.venues;
+          //searchResults.events = data.data._embedded.events[0]._embedded.venues;
             console.log(data)
           res.status(200).send(searchResults);
           })
