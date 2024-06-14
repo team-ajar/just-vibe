6b1acfb06a6937904621c6377e844d7e5bb65f24
Allyn McKinney <allynmckinney@Allyns-MacBook-Pro.local>
2024-06-14 15:33:55 -0500

    follow user front end

--- client/src/components/SearchUserResults.tsx
+++ client/src/components/SearchUserResults.tsx
@@ -2,11 +2,31 @@ import React, {useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 
+interface User {
+  id: number;
+  googleId: string;
+  location: string;
+  name: string;
+  username: string;
+  bio: string;
+  image: string;
+}
+
 const SearchUsers = () => {
   const { query } = useParams();
   const [searchResults, setSearchResults] = useState([]);
+  const [user, setUser] = useState({id: 0, googleId: '', location: '', name: '', username: '' });
 
+  const loadUser = () => {
+    axios.get('/api/user')
+      .then(({ data }: any) => {
+        setUser(data);
+      })
+      .catch(err => console.error(err));
+  };
   useEffect(() => {
+    loadUser();
+
     fetch(`/api/search/users/${query}`)
       .then((data: any) => data.json())
       .then((response: any) => {
@@ -15,12 +35,21 @@ const SearchUsers = () => {
       .catch((err: any) => console.error(err));
   }, [query]);
 
+  const followUser = (following: any) => {
+    axios.post(`/api/follow/${user.id}/${following}`)
+      .then(data => console.log)
+      .catch(err => console.error(err));
+  };
+
+
 return (
   <div>
     <h1>People:</h1>
     <ul>
       {searchResults.map((user: any, index: number) => (
-        <li key={index}>{`@${user.username} - ${user.name}`}</li>
+        <li key={index}>{`@${user.username} - ${user.name}`}
+          <button onClick={() => followUser(user.id)}>Follow</button>
+        </li>
       ))}
     </ul>
   </div>
