23b1358e1bd9eeaa0aa07ce9f66aca0483b3873d
Allyn McKinney <allynmckinney@Allyns-MacBook-Pro.local>
2024-06-13 23:35:10 -0500

    dynamic render user search results

--- client/src/components/SearchUserResults.tsx
+++ client/src/components/SearchUserResults.tsx
@@ -5,14 +5,23 @@ import axios from 'axios';
 const SearchUsers = () => {
   const { query } = useParams();
   const [searchResults, setSearchResults] = useState([]);
+
   useEffect(() => {
-    fetch(`/api/search/${query}`)
-  })
+    fetch(`/api/search/users/${query}`)
+      .then((data: any) => data.json())
+      .then((response: any) => {
+        setSearchResults(response);
+      })
+      .catch((err: any) => console.error(err));
+  }, [query]);
+
 return (
   <div>
     <h1>People:</h1>
     <ul>
-      <li>Hello</li>
+      {searchResults.map((user: any) => (
+        <li>{`@${user.username} - ${user.name}`}</li>
+      ))}
     </ul>
   </div>
 )
