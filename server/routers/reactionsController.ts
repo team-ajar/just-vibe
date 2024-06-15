4ac02c13e0d77ed212003fe7d9a5b3d3ab424c16
uhrinuh <nasri.arina@gmail.com>
2024-06-15 08:56:47 -0700

    updated schema and created backend for reactions

---
+++ server/routers/reactionsController.ts
@@ -0,0 +1,46 @@
+import { PrismaClient } from "@prisma/client";
+import { Request, Response } from "express";
+
+const prisma = new PrismaClient();
+
+const reactionsController = {
+
+  addReaction: (req: Request, res: Response) => {
+    const { emoji, userId, postId } = req.body;
+
+    prisma.reaction.create({
+      data: {
+        emoji,
+        userId: parseInt(userId),
+        postId: parseInt(postId),
+      },
+    })
+    .then(() => {
+      res.sendStatus(201)
+    })
+    .catch((err) => {
+      console.error('Error adding reaction', err);
+      res.sendStatus(500);
+    })
+  },
+
+  removeReaction: (req: Request, res: Response) => {
+    const { userId, postId } = req.params;
+
+    prisma.reaction.deleteMany({
+      where: {
+        userId: parseInt(userId),
+        postId: parseInt(postId)
+      },
+    })
+    .then(() => {
+      res.sendStatus(200);
+    })
+    .catch((err) => {
+      console.error('Error removing reaction', err);
+      res.sendStatus(500);
+    })
+  },
+}
+
+export default reactionsController;
