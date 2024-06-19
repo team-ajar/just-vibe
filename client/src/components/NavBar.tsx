b024dd549d5787bbd220ca6740ceae53825c5cc5
Allyn McKinney <allynmckinney@Allyns-MacBook-Pro.local>
2024-06-18 19:00:48 -0500

    change nav button

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,43 +1,52 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import LightDarkMode from './LightDarkMode';
-import { AppBar, Box, IconButton, Typography, Container, Button, Toolbar, InputBase } from '@mui/material';
-import SearchIcon from '@mui/icons-material/Search';
-import { styled, alpha } from '@mui/material/styles';
-import logo from './justvibelogo.png';
-import theme from '../theme';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import LightDarkMode from "./LightDarkMode";
+import {
+  AppBar,
+  Box,
+  IconButton,
+  Typography,
+  Container,
+  Button,
+  Toolbar,
+  InputBase,
+} from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import { styled, alpha } from "@mui/material/styles";
+import logo from "./justvibelogo.png";
+import theme from "../theme";
 
-const pages = ['Profile', 'Feed'];
+const pages = ["Profile", "Feed"];
 
-const Search = styled('div')(({ theme }) => ({
-  position: 'relative',
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
   backgroundColor: alpha(theme.palette.common.white, 0.15),
   marginRight: theme.spacing(2),
 }));
 
-const SearchIconWrapper = styled('div')(({ theme }) => ({
+const SearchIconWrapper = styled("div")(({ theme }) => ({
   padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  display: 'flex',
-  alignItems: 'center',
+  height: "100%",
+  position: "absolute",
+  display: "flex",
+  alignItems: "center",
 }));
 
 const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: 'inherit',
-  '& .MuiInputBase-input': {
+  color: "inherit",
+  "& .MuiInputBase-input": {
     paddingLeft: theme.spacing(6),
   },
 }));
 
 const NavBar = () => {
-  const [searchQuery, setSearchQuery] = useState('');
+  const [searchQuery, setSearchQuery] = useState("");
 
   return (
-    <AppBar position="static" sx={{ backgroundColor: '#CB6CE6' }}>
+    <AppBar position="static" sx={{ backgroundColor: "#CB6CE6" }}>
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          <Box sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }}>
+          <Box sx={{ display: { xs: "none", md: "flex" }, mr: 1 }}>
             <img src={logo} alt="Just Vibe Logo" height="100" />
           </Box>
           <Typography
@@ -47,19 +56,22 @@ const NavBar = () => {
             sx={{
               mr: 2,
               ...theme.typography.h6,
-              color: 'inherit',
-              textDecoration: 'none',
+              color: "inherit",
+              textDecoration: "none",
             }}
           >
             Just Vibe
           </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
             {pages.map((page) => (
               <Button
+                color="primary"
+                variant="contained"
                 key={page}
-                sx={{ color: 'white', display: 'block' }}
+                sx={{ color: "white", display: "block", m: 1 }}
                 component={Link}
                 to={`/${page.toLowerCase()}`}
+                
               >
                 {page}
               </Button>
@@ -79,7 +91,7 @@ const NavBar = () => {
               aria-label="search"
               component={Link}
               to={`/search-results/${searchQuery}`}
-              onClick={() => setSearchQuery('')}
+              onClick={() => setSearchQuery("")}
             >
               <SearchIcon />
             </IconButton>
