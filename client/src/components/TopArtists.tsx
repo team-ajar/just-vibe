fe672552a11db0f2e51612a8b6c6becaf075578b
cougarred1 <RodmanLavalais@gmail.com>
2024-07-11 19:17:31 -0500

    UPDATED pushes / profile and search result & first test progress

--- client/src/components/TopArtists.tsx
+++ client/src/components/TopArtists.tsx
@@ -49,14 +49,15 @@ export const TopArtistsComponent = () => {
       <Box sx={{ display: "flex", gap: 3, flexWrap: "wrap", pt: 2 }}>
         {topArtists.map((topArtist, index) => (
           <Card key={index} sx={{ width: 300, boxShadow: "5px 5px 0px #000", border: "2px solid #000" }}>
-            <CardContent sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+            <CardContent sx={{ display: "flex", flexDirection: "column", alignItems: "center", position: "relative", pt: "25%" }}>
+              
               <Typography variant="h3" sx={{ mt: 1 }}>{topArtist?.artist.name || "Select Artist"}</Typography>
               <Typography variant="h3" sx={{ mt: 1 }}>#{index + 1}</Typography>
               <Button
                 variant="contained"
                 color="primary"
                 onClick={() => deleteSelectedArtist(topArtist?.artist.id)}
-                sx={{ mt: 2 }}
+                sx={{ mt: 2, position: "absolute", top: 0, right: "10px" }}
               >
                 Delete
               </Button>
